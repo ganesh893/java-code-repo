c1225367b34150c20e16ae5a28b3778544ba6ec2
ganesh <ganesh.gp94@gmail.com>
2021-09-18 06:56:26 +0530

    Upcasting demo program added

--- InterivewQuetions/Casting1.java
+++ InterivewQuetions/Casting1.java
@@ -1,10 +1,16 @@
 package InterivewQuetions;
 
-public class Casting1 {
+public class Casting1 extends Casting2{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+      Casting1 c1= new Casting1();
+      Casting2 c=(Casting2)c1;
+      System.out.println("correct");
+      
+      Object o= new String("Ganesh");
+      String s= (String)o;
+      System.out.println(s);
+      
 	}
 
 }
